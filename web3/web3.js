7143eb20730dc72912ae648cce59fb6948db656f
Stan <iamjacke@gmail.com>
2017-11-26 13:24:43 +0300

    Pre-release stage was completed

--- web3/web3.js
+++ web3/web3.js
@@ -1,7 +1,6 @@
 
 var blockNumber;
-var endpoint = process.argv.slice(2)[1];
-
+var endpoint = process.argv.slice(2)[1]
 // http://192.168.42.1:8545
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider(endpoint));
@@ -12,4 +11,12 @@ console.log(process.argv.slice(2))
 web3.eth.getBlock(process.argv.slice(2)[0], (error, result) => {
   console.log('error:', error);
   console.log('results', result);
+});
+
+web3.eth.getBalance("0xE6FB9edCfe6C11A1f2e385ea513d08Ca0A0F24af").then((balance) => {
+	console.log(
+	`
+	Account balance: ${balance}
+	`
+	)
 });
